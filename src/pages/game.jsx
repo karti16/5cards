12b217d533bd77badc4dd9d96e453f5a5ee5f9d6
karti16 <kartim316@gmail.com>
2024-12-22 21:01:17 +0530

    color-correction

--- src/pages/game.jsx
+++ src/pages/game.jsx
@@ -7,23 +7,44 @@ import { useEffect, useState } from 'react';
 import { db } from '../db';
 import { cn } from '@/lib/utils';
 import { Check, FileInput } from 'lucide-react';
+import { Skeleton } from '@/components/ui/skeleton';
 
 export default function Game() {
   let params = useParams();
   const [_players, _setPlayers] = useState([]);
+  const [loading, setLoading] = useState(true);
   const fetchPlayers = async () => {
-    const data = await db
-      .select()
-      .from(players)
-      .where(and(eq(players.group_id, params.groupId), eq(players.isPlaying, 1)))
-      .orderBy(asc(players.points));
-    _setPlayers(data);
+    try {
+      const data = await db
+        .select()
+        .from(players)
+        .where(and(eq(players.group_id, params.groupId), eq(players.isPlaying, 1)))
+        .orderBy(asc(players.points));
+      _setPlayers(data);
+    } catch {
+      //
+    } finally {
+      // setLoading(false);
+    }
   };
 
   useEffect(() => {
+    setLoading(true);
+
     fetchPlayers();
   }, [params]);
 
+  // if (loading) {
+  //   return (
+  //     <div className='flex items-center space-x-4'>
+  //       <Skeleton className='h-12 w-12 rounded-full' />
+  //       <div className='space-y-2'>
+  //         <Skeleton className='h-4 w-[250px]' />
+  //         <Skeleton className='h-4 w-[200px]' />
+  //       </div>
+  //     </div>
+  //   );
+  // }
   return (
     <div className='p-6'>
       <Header groupId={params.groupId} />
@@ -68,7 +89,10 @@ export default function Game() {
                   </div>
                 );
               })}
-            <hr className='h-px my-8 bg-gray-200 border-0 dark:bg-gray-700'></hr>
+            <span className='text-gray-600 text-sm'>
+              No. of players still in game : {_players.filter((i) => i.points < 100).length}
+            </span>
+            <hr className='h-px my-4 bg-gray-200 border-0 dark:bg-gray-700'></hr>
             <p className='flex pb-5'>Out of the Game</p>
             {_players
               .filter((i) => i.points >= 100)
