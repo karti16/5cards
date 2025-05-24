dcf9f7385d6c8b0e64f338bf2f97a5e158b911bc
karti16 <kartim316@gmail.com>
2025-05-25 01:26:16 +0530

    fix route

--- server/index.js
+++ server/index.js
@@ -15,8 +15,11 @@ app.use('/api/group', groupRouter);
 app.use('/api/player', playerRouter);
 app.use('/api/round', roundsRouter);
 
-
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+app.get(/(.*)/, (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
